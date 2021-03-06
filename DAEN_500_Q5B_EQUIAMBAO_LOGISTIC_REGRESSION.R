55349643a021ea462161b5bd1fddc4253ae7e5ed
EmmanuelQuiambao <78194013+EmmanuelQuiambao@users.noreply.github.com>
2021-03-06 13:40:34 -0500

    Add files via upload

---
+++ DAEN_500_Q5B_EQUIAMBAO_LOGISTIC_REGRESSION.R
@@ -0,0 +1,24 @@
+
+library (broom)
+
+#Load WHO stroke prediction data set downloaded from https://www.kaggle.com/fedesoriano/stroke-prediction-dataset
+analytic2 <- read.csv(file="healthcare-dataset-stroke-data.csv", header=TRUE, sep=",")
+
+
+
+plot(analytic2$age, analytic2$stroke, xlab='age',ylab='Has Had a Stroke')
+
+model = glm(formula = stroke ~ age, family = binomial, data = analytic2)
+summary(model)
+
+
+
+mod.hit = model
+pihat = mod.hit$fitted.values
+pihat[1:10]
+evresids = residuals(mod.hit)
+resids = residuals(mod.hit, type = "response")
+Presids = residuals(mod.hit, type = "pearson")
+plot(pihat, Presids, xlab = "Predicted probability", ylab = "Pearson residual")
+
+
