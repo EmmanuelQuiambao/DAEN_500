d2cb870eb36af8757bb30fbab59f681171789111
EmmanuelQuiambao <78194013+EmmanuelQuiambao@users.noreply.github.com>
2021-03-06 21:16:42 -0500

    Add files via upload

---
+++ DAEN_500_Q3_EQUIAMBAO.R
@@ -0,0 +1,6 @@
+myCourses <- c("Math101","English101","Science101","Spanish101","Algebra101","Music101","Nutrition101","Histor101")
+length(courses)
+myCourses[1:2]
+myCourses[3:4]
+sort(myCourses)
+sort(myCourses, decreasing = TRUE)
